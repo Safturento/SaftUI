12e5f5f50fa8760cba79e7de03af0f7dbf047496
Safturento <safturento@gmail.com>
2019-11-13 12:42:11 -0600

    lock config from combat to avoid taint

--- Core/init.lua
+++ Core/init.lua
@@ -6,12 +6,13 @@ SlashCmdList.RELOADUI = _G.ReloadUI
 SaftUI = LibStub('AceAddon-3.0'):NewAddon(
 	st, ADDON_NAME, 'AceEvent-3.0', 'AceHook-3.0')
 
+local ACD = LibStub('AceConfigDialog-3.0')
 st.StringFormat = LibStub('LibStringFormat-1.0')
 
 function SaftUI:OnInitialize()
-	-- SetCVar("useUiScale", 1)
-	-- SetCVar("uiScale", 768/string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)"))
-
+	SetCVar("useUiScale", 1)
+	SetCVar("uiScale", st.ui_scale)
+	
 	self.config = LibStub('AceDB-3.0'):New('SaftUI_DB', {
 		char = {},
 		realm = {},
@@ -31,17 +32,19 @@ function SaftUI:OnInitialize()
 	self.config.RegisterCallback(self, 'OnProfileReset', 'UpdateConfig')
 
 	self:RegisterEvent('TIME_PLAYED_MSG')
+	self:RegisterEvent('PLAYER_REGEN_ENABLED')
+	self:RegisterEvent('PLAYER_REGEN_DISABLED')
 	RequestTimePlayed()
 
 	total_time = 0
 	for realm, realm_config in pairs(SaftUI_DB.realm) do
 		if realm_config.summary then
 			for character, summary in pairs(realm_config.summary) do
-				total_time = total_time + summary.time_played
+				total_time = total_time + (summary.time_played or 0)
 			end
 		end
 	end
-	print('You have played for a total of ' .. SecondsToTime(total_time) .. ' across your characters.')
+	-- print('You have played for a total of ' .. SecondsToTime(total_time) .. ' across your characters.')
 end
 
 function SaftUI:TIME_PLAYED_MSG(event, total, level)
@@ -57,6 +60,22 @@ function SaftUI:UpdateConfig()
 	end
 end
 
+local config_queued = false
+function  SaftUI:PLAYER_REGEN_ENABLED()
+	if config_queued then
+		config_queued = false
+		SlashCmdList.SAFTUI()
+	end
+end
+
+function SaftUI:PLAYER_REGEN_DISABLED()
+	if ACD.OpenFrames[ADDON_NAME] then
+		st:Print('Config closed for combat, and will reopen after.')
+		config_queued = true
+		ACD:Close(ADDON_NAME)
+	end
+end
+
 SLASH_SAFTUI1 = '/saftui'
 SLASH_SAFTUI2 = '/sui'
 SLASH_SAFTUI3 = '/stui'
@@ -66,5 +85,10 @@ SlashCmdList.SAFTUI = function(msg)
 		st.CF:InitializeConfigGUI()
 	end
 
-	LibStub('AceConfigDialog-3.0'):Open(ADDON_NAME)
+	if InCombatLockdown() then
+		st:Print('Config will open after combat')
+		config_queued = true
+	else
+		ACD:Open(ADDON_NAME)
+	end
 end
